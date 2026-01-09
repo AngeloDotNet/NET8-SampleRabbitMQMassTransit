a2f22c1dacf38ad370334b6b0d948ada0196f76e
Angelo Pirola <angelo9871@gmail.com>
2026-01-10 00:01:19 +0100

    Add BusStarterHostedService to manage RabbitMQ bus lifecycle
    Introduced BusStarterHostedService in SampleMicroservice.Messaging, implementing IHostedService to start and stop the IRabbitMqBus with application lifecycle events. Added required using directives for hosting support.

---
+++ src/SampleMicroservice.Messaging/BusStarterHostedService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace SampleMicroservice.Messaging;
+
+public class BusStarterHostedService(IRabbitMqBus bus) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        bus.Start();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        bus.Stop();
+        return Task.CompletedTask;
+    }
+}

da00b84a9158068a6ec1c9d3693b286d01a1473e
Angelo Pirola <angelo9871@gmail.com>
2026-01-10 00:01:30 +0100

    Add DI extension for RabbitMQ bus registration
    Introduced DependencyInjection static class with an AddRabbitMqBus extension method for IServiceCollection. This enables easy registration of IRabbitMqBus (RabbitMqBus) with required connection parameters and logger support. Includes commented alternatives for registration approaches.

---
+++ src/SampleMicroservice.Messaging/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SampleMicroservice.Messaging;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddRabbitMqBus(this IServiceCollection services, string host, string username, string password, string virtualHost)
+    {
+        //services.AddSingleton<IRabbitMqBus>(sp => new RabbitMqBus(host, username, password, virtualHost, logger));
+
+        services.AddSingleton<IRabbitMqBus>(sp =>
+        {
+            var logger = sp.GetRequiredService<ILogger<RabbitMqBus>>();
+
+            return new RabbitMqBus(host, username, password, virtualHost, logger);
+        });
+
+        //services.AddSingleton<RabbitMqBus>(sp =>
+        //{
+        //    var logger = sp.GetRequiredService<ILogger<RabbitMqBus>>();
+        //    return new RabbitMqBus(host, username, password, virtualHost, logger);
+        //});
+        //services.AddSingleton<IRabbitMqBus>(sp => sp.GetRequiredService<RabbitMqBus>());
+
+        return services;
+    }
+}

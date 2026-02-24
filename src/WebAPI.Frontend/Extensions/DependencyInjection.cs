07084dfad9a474735497a8fb9442702956e7d837
Angelo Pirola <angelo9871@gmail.com>
2026-02-25 00:10:23 +0100

    Refactor RabbitMQ DI to use IRabbitMqBus singleton
    Replaced MassTransit setup with IRabbitMqBus singleton registration using constructor parameters. Removed queue configuration and hardcoded settings. Added BusStarterHostedService for automatic bus management. Updated AddFrontEndRabbitMQ method signature.

--- src/WebAPI.Frontend/Extensions/DependencyInjection.cs
+++ src/WebAPI.Frontend/Extensions/DependencyInjection.cs
@@ -1,43 +1,20 @@
-﻿namespace WebAPI.Frontend.Extensions;
+﻿using SampleMicroservice.Messaging;
 
-/// <summary>
-/// Contains extension methods for dependency injection.
-/// </summary>
+namespace WebAPI.Frontend.Extensions;
+
 public static class DependencyInjection
 {
-    /// <summary>
-    /// Adds RabbitMQ as a message broker using MassTransit.
-    /// </summary>
-    /// <param name="services">The IServiceCollection to add services to.</param>
-    /// <param name="vHost">The virtual host to connect to on the RabbitMQ server.</param>
-    /// <param name="queueName">The name of the queue to use.</param>
-    /// <returns>The same service collection so that multiple calls can be chained.</returns>
-    public static IServiceCollection AddFrontEndRabbitMQ(this IServiceCollection services, string vHost, string queueName)
+    public static IServiceCollection AddFrontEndRabbitMQ(this IServiceCollection services, string host, string username, string password, string virtualHost)
     {
-        services.AddMassTransit(x =>
+        // Register bus singleton using Settings values and resolve ILogger<RabbitMqBus> from DI
+        services.AddSingleton<IRabbitMqBus>(sp =>
         {
-            x.UsingRabbitMq((context, rabbit) =>
-            {
-                rabbit.QueueExpiration = TimeSpan.FromSeconds(Settings.QueueExpiration);
-                rabbit.Host(Settings.RabbitMQHost, vHost, h =>
-                {
-                    h.Username(Settings.RabbitMQUsername);
-                    h.Password(Settings.RabbitMQPassword);
-                });
-
-                rabbit.ReceiveEndpoint(queueName, e =>
-                {
-                    e.Durable = Settings.Durable;
-                    e.AutoDelete = Settings.AutoDelete;
-                    e.ExchangeType = Settings.ExchangeType;
-                    e.PrefetchCount = Settings.PrefetchCount;
-
-                    e.UseMessageRetry(r => r.Interval(Settings.RetryCount, Settings.RetryInterval));
-                });
-            });
-
-            x.AddRequestClient(typeof(PersonRequest));
+            var logger = sp.GetRequiredService<ILogger<RabbitMqBus>>();
+            return new RabbitMqBus(host, username, password, virtualHost, logger);
         });
+
+        // add hosted service to start/stop the bus automatically
+        services.AddHostedService<BusStarterHostedService>();
 
         return services;
     }

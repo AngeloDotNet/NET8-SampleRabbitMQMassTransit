bbcedd7c6d718c4300a751f33750530efded6f88
Angelo Pirola <angelo9871@gmail.com>
2026-02-25 00:08:43 +0100

    Remove XML doc comments from PeopleController actions
    All XML documentation comments were removed from PeopleController.cs. The controller action methods themselves were not modified.

--- src/WebAPI.Backend/Controllers/PeopleController.cs
+++ src/WebAPI.Backend/Controllers/PeopleController.cs
@@ -1,16 +1,9 @@
 ﻿namespace WebAPI.Backend.Controllers;
 
-/// <summary>
-/// Controller for managing people.
-/// </summary>
 public class PeopleController(IPeopleService peopleService) : BaseController
 {
     private readonly IPeopleService peopleService = peopleService;
 
-    /// <summary>
-    /// Gets a list of all people.
-    /// </summary>
-    /// <returns>A list of all people.</returns>
     [HttpGet]
     public async Task<IActionResult> GetPeopleAsync()
     {
@@ -18,11 +11,6 @@ public class PeopleController(IPeopleService peopleService) : BaseController
         return Ok(people);
     }
 
-    /// <summary>
-    /// Gets a specific person by their ID.
-    /// </summary>
-    /// <param name="id">The ID of the person.</param>
-    /// <returns>The person with the given ID, or NotFound if no such person exists.</returns>
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetPersonAsync(int id)
     {
@@ -36,11 +24,6 @@ public class PeopleController(IPeopleService peopleService) : BaseController
         return Ok(person);
     }
 
-    /// <summary>
-    /// Creates a new person.
-    /// </summary>
-    /// <param name="entity">The person to create.</param>
-    /// <returns>Ok if the person was created successfully.</returns>
     [HttpPost]
     public async Task<IActionResult> CreatePersonAsync([FromBody] PersonEntity entity)
     {
@@ -48,11 +31,6 @@ public class PeopleController(IPeopleService peopleService) : BaseController
         return Ok();
     }
 
-    /// <summary>
-    /// Edits an existing person.
-    /// </summary>
-    /// <param name="entity">The person to edit.</param>
-    /// <returns>Ok if the person was edited successfully.</returns>
     [HttpPut]
     public async Task<IActionResult> EditPersonAsync([FromBody] PersonEntity entity)
     {
@@ -60,11 +38,6 @@ public class PeopleController(IPeopleService peopleService) : BaseController
         return Ok();
     }
 
-    /// <summary>
-    /// Deletes a specific person by their ID.
-    /// </summary>
-    /// <param name="id">The ID of the person to delete.</param>
-    /// <returns>Ok if the person was deleted successfully, or NotFound if no such person exists.</returns>
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeletePersonAsync(int id)
     {
